866c3e058ebb1fedab742367f5d98cd29a07f7ba
Charlotte Mackin <cmackin@cshnyc.org>
2018-09-18 13:53:49 -0400

    9/18/18 class notes

---
+++ helloworld.py
@@ -0,0 +1,32 @@
+#wizard_list = 'spider legs, toe of frog, eye of newt, bat wing, slug butter, snake dandruff'
+#print(wizard_list)
+
+#wizard_list = ['spider legs', 'toe of frog', 'eye of newt', 'bat wing', 'slug butter', 'snake dandruff']
+#print(wizard_list)
+#lists are more useful than strings
+#combination of integers and strings
+#print(wizard_list[2])
+
+dog_list = ['golden retriever', 3, 'cavalier king charles', 2, 'chihuahua', 1, 'golden doodle', 2, 'cockapoo', 5]
+print(dog_list)
+#adding items to a list we used the append function
+dog_list.append('the newest list item')
+print(dog_list)
+
+#deleting: similar to append function
+del dog_list[5]
+print(dog_list)
+
+#adding and multiplying lists
+myList1 = (1,2,3,4)
+myList2 = (5,6,7,8)
+myListAdd = myList1 + myList2
+print(myListAdd)
+myListmult = myList1 * 4
+print(myListmult)
+#cannot do this with dividing or subracting, you would get an error
+#example:
+    #myListdiv = myList1 /4
+    #print(myListdiv)
+
+#NOTE: these are all data types
